668af4edbdd0307d8a11d60d57989c4f14dc9d5b
regicid <benoit.de.courson@ens.fr>
2024-08-15 18:23:09 +0200

    bla

--- app.py
+++ app.py
@@ -128,9 +128,9 @@ def query():
         except:
             by_rubrique = False
         print(by_rubrique)
-        if " " in rubrique and len(rubrique.split(' ') < 8:
+        if " " in rubrique and len(rubrique.split(' ')) < 8:
             rubrique_condition = f"and rubrique in {tuple(rubrique.split(' '))}"
-        else if len(rubrique.split(' ') == 8:
+        else if len(rubrique.split(' ')) == 8:
             rubrique_condition == ""
         else:
             rubrique_condition = f"and rubrique=\"{rubrique}\""
